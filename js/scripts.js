8bcd2fc80b5516430352a88ea56bd0853d388166
Brightmuk <be.rightmuk@gmail.com>
2020-11-17 04:45:50 +0300

    Add scroll indicator

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,14 @@
 $(document).ready(function(){
     $(window).scroll(function(){
-        console.log(this.scrollY);
         if(this.scrollY > 20){
+            scrollIndication();
             $('.navbar').addClass('sticky');
+            $('.progress-container').css("display","block");
+            $('.progress-bar').css("display","block");
         }else{
             $('.navbar').removeClass('sticky');
+            $('.progress-container').css("display","none");
+            $('.progress-bar').css("display","none");
         }
     });
     // toggle menu script
@@ -13,3 +17,11 @@ $(document).ready(function(){
         $('.menu-btn i').toggleClass('active');
     });
 })
+
+//scroll indicator
+function scrollIndication() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+  }
