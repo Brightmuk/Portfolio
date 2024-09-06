5866f774d8f2b3c4ec9c46486831a42ea6af2282
Brimuk <be.rightmuk@gmail.com>
2024-09-06 16:25:10 +0300

    Standardize svg sizes

--- src/svg/top-right.js
+++ src/svg/top-right.js
@@ -3,7 +3,7 @@ import '../App.css';
 export default function TopRight(props) {
     var color = props.on?'#e85d04':'none';
     return (
-        <svg className='top-right bit' width="23" height="84" viewBox="0 0 76 269" fill={color} xmlns="http://www.w3.org/2000/svg">
+        <svg className='top-right bit'viewBox="0 0 76 269" fill={color} xmlns="http://www.w3.org/2000/svg">
             <path d="M60.5 1L1 60.5V232.5L36.5 267.5L75 229V15.5L60.5 1Z" stroke="black" />
         </svg>
     )
--- src/svg/top.js
+++ src/svg/top.js
@@ -3,7 +3,7 @@ import '../App.css';
 export default function Top(props) {
     var color = props.on?'#e85d04':'none';
     return (
-        <svg className='top bit' width="91" height="23" viewBox="0 0 294 75" fill={color} xmlns="http://www.w3.org/2000/svg">
+        <svg className='top bit' viewBox="0 0 294 75" fill={color} xmlns="http://www.w3.org/2000/svg">
             <path d="M279 0.5H15L1 14.5L60.5 74.5H232.5L293 14.5L279 0.5Z" stroke="black" />
         </svg>
     )
