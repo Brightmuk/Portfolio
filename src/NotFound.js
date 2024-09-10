0a68ae3a5b1015605140ccc904ebf255f41f85de
Brimuk <be.rightmuk@gmail.com>
2024-09-10 19:32:43 +0300

    Add not found page

--- src/NotFound.js
+++ src/NotFound.js
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+import './css/App.css';
 
 export default function NotFound(){
     return (
         <div className="not-found">
-            <h2>Not Found</h2>
+            <h1>OOps!</h1>
+            <h2>Page Not Found</h2>
+            <p>Go back <Link className="link" to ="/">Home</Link></p>
         </div>
     );
 }
