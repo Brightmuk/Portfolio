b906a18b2133daa456f28c65d579905ee0818f4b
Brimuk <be.rightmuk@gmail.com>
2024-11-13 21:18:08 +0300

    save commit

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -11,7 +11,7 @@ import TopRight from '../svg/top-right';
 
 export default function Clock() {
     const [time, SetTime] = useState(defaultTime());
-
+    const date = new Date()
     useEffect(() => {
 
         const interval = setInterval(() => {
@@ -25,67 +25,68 @@ export default function Clock() {
     }, []);
     return (
         <div className='clock'>
+           
             <div id="first" className='digit'>
-                <Top on={time.first['top']} />
-                <TopLeft on={time.first['top-left']} className='bit' />
-                <TopRight on={time.first['top-right']} className='bit' />
-                <Center on={time.first['center']} className='bit' />
-                <BottomLeft on={time.first['bottom-left']} className='bit' />
-                <BottomRight on={time.first['bottom-right']} className='bit' />
-                <Bottom on={time.first['bottom']} className='bit' />
+                <Top on={time.first.top} />
+                <TopLeft on={time.first.topLeft} className='bit' />
+                <TopRight on={time.first.topRight} className='bit' />
+                <Center on={time.first.center} className='bit' />
+                <BottomLeft on={time.first.bottomLeft} className='bit' />
+                <BottomRight on={time.first.bottomRight} className='bit' />
+                <Bottom on={time.first.bottom} className='bit' />
             </div>
             <div id="second" className='digit'>
-                <Top on={time.second['top']} />
-                <TopLeft on={time.second['top-left']} className='bit' />
-                <TopRight on={time.second['top-right']} className='bit' />
-                <Center on={time.second['center']} className='bit' />
-                <BottomLeft on={time.second['bottom-left']} className='bit' />
-                <BottomRight on={time.second['bottom-right']} className='bit' />
-                <Bottom on={time.second['bottom']} className='bit' />
+                <Top on={time.second.top} />
+                <TopLeft on={time.second.topLeft} className='bit' />
+                <TopRight on={time.second.topRight} className='bit' />
+                <Center on={time.second.center} className='bit' />
+                <BottomLeft on={time.second.bottomLeft} className='bit' />
+                <BottomRight on={time.second.bottomRight} className='bit' />
+                <Bottom on={time.second.bottom} className='bit' />
             </div>
             <div className='colon'>
                 <div className='box'></div>
                 <div className='box'></div>
             </div>
             <div id="third" className='digit'>
-                <Top on={time.third['top']} />
-                <TopLeft on={time.third['top-left']} className='bit' />
-                <TopRight on={time.third['top-right']} className='bit' />
-                <Center on={time.third['center']} className='bit' />
-                <BottomLeft on={time.third['bottom-left']} className='bit' />
-                <BottomRight on={time.third['bottom-right']} className='bit' />
-                <Bottom on={time.third['bottom']} className='bit' />
+                <Top on={time.third.top} />
+                <TopLeft on={time.third.topLeft} className='bit' />
+                <TopRight on={time.third.topRight} className='bit' />
+                <Center on={time.third.center} className='bit' />
+                <BottomLeft on={time.third.bottomLeft} className='bit' />
+                <BottomRight on={time.third.bottomRight} className='bit' />
+                <Bottom on={time.third.bottom} className='bit' />
             </div>
             <div id="fourth" className='digit'>
-                <Top on={time.fourth['top']} />
-                <TopLeft on={time.fourth['top-left']} className='bit' />
-                <TopRight on={time.fourth['top-right']} className='bit' />
-                <Center on={time.fourth['center']} className='bit' />
-                <BottomLeft on={time.fourth['bottom-left']} className='bit' />
-                <BottomRight on={time.fourth['bottom-right']} className='bit' />
-                <Bottom on={time.fourth['bottom']} className='bit' />
+                <Top on={time.fourth.top} />
+                <TopLeft on={time.fourth.topLeft} className='bit' />
+                <TopRight on={time.fourth.topRight} className='bit' />
+                <Center on={time.fourth.center} className='bit' />
+                <BottomLeft on={time.fourth.bottomLeft} className='bit' />
+                <BottomRight on={time.fourth.bottomRight} className='bit' />
+                <Bottom on={time.fourth.bottom} className='bit' />
             </div>
             <div className='colon'>
                 <div className='box'></div>
                 <div className='box'></div>
             </div>
             <div id="fifth" className='digit'>
-                <Top on={time.fifth['top']} />
-                <TopLeft on={time.fifth['top-left']} className='bit' />
-                <TopRight on={time.fifth['top-right']} className='bit' />
-                <Center on={time.fifth['center']} className='bit' />
-                <BottomLeft on={time.fifth['bottom-left']} className='bit' />
-                <BottomRight on={time.fifth['bottom-right']} className='bit' />
-                <Bottom on={time.fifth['bottom']} className='bit' />
+                <Top on={time.fifth.top} />
+                <TopLeft on={time.fifth.topLeft} className='bit' />
+                <TopRight on={time.fifth.topRight} className='bit' />
+                <Center on={time.fifth.center} className='bit' />
+                <BottomLeft on={time.fifth.bottomLeft} className='bit' />
+                <BottomRight on={time.fifth.bottomRight} className='bit' />
+                <Bottom on={time.fifth.bottom} className='bit' />
             </div>
             <div id="sixth" className='digit'>
-                <Top on={time.sixth['top']} />
-                <TopLeft on={time.sixth['top-left']} className='bit' />
-                <TopRight on={time.sixth['top-right']} className='bit' />
-                <Center on={time.sixth['center']} className='bit' />
-                <BottomLeft on={time.sixth['bottom-left']} className='bit' />
-                <BottomRight on={time.sixth['bottom-right']} className='bit' />
-                <Bottom on={time.sixth['bottom']} className='bit' />
+                <Top on={time.sixth.top} />
+                <TopLeft on={time.sixth.topLeft} className='bit' />
+                <TopRight on={time.sixth.topRight} className='bit' />
+                <Center on={time.sixth.center} className='bit' />
+                <BottomLeft on={time.sixth.bottomLeft} className='bit' />
+                <BottomRight on={time.sixth.bottomRight} className='bit' />
+                <Bottom on={time.sixth.bottom} className='bit' />
             </div>
         </div>
     )
